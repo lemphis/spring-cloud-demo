e8dd481737986ab66941cb67167e4265de048c62
lemphis <lemphis@gmail.com>
2022-12-08 22:44:54 +0900

    Initial commit

---
+++ eureka-client/src/main/kotlin/me/lemphis/eurekaclient/EurekaClientApplication.kt
@@ -0,0 +1,11 @@
+package me.lemphis.eurekaclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<EurekaClientApplication>(*args)
+}
---
+++ src/main/kotlin/me/lemphis/springclouddemo/SpringCloudDemoApplication.kt
@@ -0,0 +1,11 @@
+package me.lemphis.springclouddemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudDemoApplication>(*args)
+}
