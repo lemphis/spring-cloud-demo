4934f9f0760ae687450237eda4f87748fc0b5c81
lemphis <lemphis@gmail.com>
2022-12-11 18:18:16 +0900

    Wip
    * 오타 수정
    * 각 설정을 컨테이너 환경에 맞게 변경 (IP -> container name)
    * docker-compose.yml에 의존 관계 추가
    * eureka-client에 test api 추가

--- eureka-client/src/main/kotlin/me/lemphis/eurekaclient/EurekaClientApplication.kt
+++ eureka-client/src/main/kotlin/me/lemphis/eurekaclient/EurekaClientApplication.kt
@@ -3,6 +3,8 @@ package me.lemphis.eurekaclient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,4 +12,15 @@ class EurekaClientApplication
 
 fun main(args: Array<String>) {
     runApplication<EurekaClientApplication>(*args)
+}
+
+@RestController
+class Apis {
+
+    @GetMapping("/test")
+    suspend fun test() = mutableMapOf<String, Any>(
+        "a" to 10,
+        "b" to 20,
+    )
+
 }
